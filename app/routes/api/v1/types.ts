fe9e88d77572a4218af91872b1178cb0fd15cc5c
Encode42 <me@encode42.dev>
2022-08-21 22:03:20 -0400

    Return types object for consistency

--- app/routes/api/v1/types.ts
+++ app/routes/api/v1/types.ts
@@ -3,5 +3,7 @@ import { getVersions } from "~/util/storage/getVersions.server";
 export async function loader() {
     const versions = await getVersions();
 
-    return versions.types;
+    return {
+        "types": versions.types
+    };
 }
