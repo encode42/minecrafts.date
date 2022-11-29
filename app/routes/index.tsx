431ec37a8c39de79caa0bc46f08fd98def79c140
Encode42 <me@encode42.dev>
2022-11-29 16:53:22 -0500

    Improve performance, fix badge bug

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useMemo, useState } from "react";
+import { memo, ReactNode, useEffect, useMemo, useState } from "react";
 import { useFetcher, useLoaderData, useLocation, useNavigate } from "@remix-run/react";
 import { Anchor, Link, RouteRequest } from "@encode42/remix-extras";
 import { ActionIcon, Group, MultiSelect, Stack, Text, TextInput, Title, CloseButton, Collapse, Button, Badge, Space, Image, Container, useMantineColorScheme } from "@mantine/core";
@@ -53,7 +53,7 @@ interface LoaderResult {
     "index": Fuse.FuseIndex<unknown> | undefined
 }
 
-function VersionTitle({ id, badges = [], released }: VersionTitleProps) {
+function _VersionTitle({ id, badges = [], released }: VersionTitleProps) {
     const anchor = (
         <Anchor to={`/${id}`}>
             <Title>{id}</Title>
@@ -77,22 +77,29 @@ function VersionTitle({ id, badges = [], released }: VersionTitleProps) {
     );
 }
 
+const VersionTitle = memo(_VersionTitle);
+
 function VersionEntry({ version, isNewest, isOldest, badges = [], onCopy }: VersionEntryProps) {
     const { colorScheme } = useMantineColorScheme();
+    const versionBadges = useMemo(() => {
+        const newBadges: string[] = badges;
 
-    if (isNewest) {
-        badges.push(`Newest ${version.type}`);
-    }
+        if (isNewest) {
+            newBadges.push(`Newest ${version.type}`);
+        }
 
-    if (isOldest) {
-        badges.push(`Oldest ${version.type}`);
-    }
+        if (isOldest) {
+            newBadges.push(`Oldest ${version.type}`);
+        }
+
+        return newBadges;
+    }, [version.type]);
 
     return (
         <LazyLoad once>
             <ThemePaper id={version.id}>
                 <Stack>
-                    <VersionTitle id={version.id} badges={badges} released={version.date.released} />
+                    <VersionTitle id={version.id} badges={versionBadges} released={version.date.released} />
                     <Group position="apart" sx={{
                         "alignItems": "flex-end"
                     }}>
@@ -142,6 +149,7 @@ export default function IndexPage() {
     const [showFilters, openFiltersHandler] = useDisclosure(false);
     const [listedVersions, setListedVersions] = useState<ReactNode[]>([]);
 
+    // TODO: Don't manipulate DOM, rather show/hide
     function createEntry(version: Version, key?: string) {
         const isNewest = version.id === data.versions.versions[data.versions.newest[version.type]].id;
         const isOldest = version.id === data.versions.versions[data.versions.oldest[version.type]].id;
@@ -225,7 +233,7 @@ export default function IndexPage() {
 
     return (
         <StandardLayout>
-            <Stack spacing="xl">
+            <Stack>
                 <ImportantPaper>
                     <Stack>
                         <ImportantTitle.Wrapper custom>
